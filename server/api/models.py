9f377d660cc47998c5bd910a47156ad33ff8419d
aswin <aswinkumar300245@gmail.com>
2023-10-06 14:18:05 +0530

    Partial model created

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    pic  = models.ImageField(upload_to='profile_pics',blank=True)
+    
+    def __str__(self):
+        return self.user.username
+
+class Record(models.Model):
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    date = models.DateField()
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=100)
+
--- server/server/settings.py
+++ server/server/settings.py
@@ -72,6 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -119,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATICFILES_DIRS  = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
